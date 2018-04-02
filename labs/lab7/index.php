6c2c27d50e32bd587b56b472fefef4dfed8be2fc
Cristian Galvan <crgalvan@csumb.edu>
2018-04-02 20:18:11 +0000

    lab 7

---
+++ labs/lab7/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin Page</title>
+    </head>
+    <body>
+        <h1>Otter Mart - Admin Login</h1>
+        <form method="POST" action="loginProcess.php">
+            Username: <input type="text" name="username"/> <br />
+            Password: <input type="password" name="password"/> <br />
+            <input type="submit" name="submit" value="Login"/> <br />
+            
+        </form>
+    </body>
+</html>
