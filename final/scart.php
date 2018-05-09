cce397507a517d8842230768e18f24a77bee39fe
Cristian Galvan <crgalvan@csumb.edu>
2018-05-09 05:54:25 +0000

    final work

--- final/scart.php
+++ final/scart.php
@@ -3,15 +3,26 @@ session_start();
 include '../dbConnection.php';
 
 $dbConn = getDatabaseConnection('finalproject');
-$sql= "SELECT price FROM allitems WHERE name= ";
+$sql= "SELECT price FROM product WHERE name= ";
 echo "<!DOCTYPE html>
 <html>
     <head>
-        <title> Menu </title>
-        <link rel='stylesheet' href='css/styles.css' type='text/css' />
-        <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>
-    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
-    
+         <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+  <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
+    </head>
+    <body >
+        <nav class='navbar navbar-inverse'>
+  <div class='container-fluid'>
+    <ul class='nav navbar-nav'>
+      <li class='active'><a href='guest.php'>Home</a></li>
+      <li><a href='menu.php'>Menu</a></li>
+      <li><a href='scart.php'>Shopping Cart</a></li>
+      <li><a href='logout.php'>Logout</a></li>
+    </ul>
+  </div>
     </head>
     <body>
     
